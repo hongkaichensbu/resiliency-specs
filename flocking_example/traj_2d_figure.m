7beb5d6caeef94919c9aba280b642d69d1a7534a
hongkaichensbu <31549950+hongkaichensbu@users.noreply.github.com>
2022-05-17 17:12:23 -0400

    Fix red portion in x-axis.

--- flocking_example/traj_2d_figure.m
+++ flocking_example/traj_2d_figure.m
@@ -14,9 +14,9 @@ figure;
 subplot(1,2,1)
 plot(time(1:2:end), J_array(1:2:end), 'LineWidth',1.5);
 hold on
-plot(time(1001:2:1501), ones(1,251),'r','LineWidth',2);
-plot(time(2501:2:3001), ones(1,251),'r','LineWidth',2);
-plot(time(4001:2:4501), ones(1,251),'r','LineWidth',2);
+plot(time(1001:2:1501), ones(1,251)* min(J_array(1:2:end)),'r','LineWidth',2);
+plot(time(2501:2:3001), ones(1,251)* min(J_array(1:2:end)),'r','LineWidth',2);
+plot(time(4001:2:4501), ones(1,251)* min(J_array(1:2:end)),'r','LineWidth',2); hold off
 ylabel('$J(\mathbf{x})$','Interpreter','latex', 'FontSize', 10,'rotation',0,'HorizontalAlignment','right');
 xlabel('time [s]')
 ylim([min(J_array(1:2:end)), max(J_array(1:2:end))])
